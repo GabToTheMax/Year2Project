1c2859b78657e106180e6b0d7a8c1fd24c92b561
GabToTheMax <gabrielsawicki32@gmail.com>
2026-02-23 22:11:58 +0000

    CameraWork

--- Assets/GabStuff/Scripts/FirstPersonCamera.cs
+++ Assets/GabStuff/Scripts/FirstPersonCamera.cs
@@ -9,6 +9,8 @@ namespace GabStuff.Scripts
         [SerializeField] private GameObject playerCamera;
         [SerializeField] private float xSensitivity;
         [SerializeField] private float ySensitivity;
+        public float facingDirection;
+        public Vector3 forwardVector3;
         private Vector2 _mouseAim;
         
         
@@ -20,13 +22,27 @@ namespace GabStuff.Scripts
         public void OnMouseMove(InputAction.CallbackContext context)
         {
             _mouseAim = context.ReadValue<Vector2>();
-            print(_mouseAim);
-            cameraCentre.transform.RotateAround(cameraCentre.transform.position, Vector3.up, _mouseAim.x*xSensitivity);
-            playerCamera.transform.RotateAround(cameraCentre.transform.position, Vector3.forward, _mouseAim.y*ySensitivity);
+            facingDirection = cameraCentre.transform.rotation.eulerAngles.y;
+            transform.RotateAround(transform.position, Vector3.up, _mouseAim.x * xSensitivity);
+            
+            // UpdateFacingVector();
+            // cameraCentre.transform.RotateAround(transform.position, Quaternion.AngleAxis(90, Vector3.up)*forwardVector3, _mouseAim.y * -ySensitivity);
+            // var cameraRotation = cameraCentre.transform.rotation;
+            Quaternion cameraRotation = new();
+            cameraRotation.eulerAngles = new Vector3(cameraCentre.transform.rotation.x + _mouseAim.y * ySensitivity, 0, 0);
+            cameraCentre.transform.rotation = cameraRotation;
+
         }
 
-        void Update()
+        void FixedUpdate()
         {
+            UpdateFacingVector();
+        }
+
+        private void UpdateFacingVector()
+        {
+            forwardVector3 = Quaternion.AngleAxis(facingDirection, Vector3.up)*Vector3.forward;
+            Debug.DrawLine(transform.position + Vector3.zero, transform.position + forwardVector3, Color.yellow);
         }
     }
 }
