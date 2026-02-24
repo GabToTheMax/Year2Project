cc7340154e59703d86667e893f306d8e44fcd6c4
GabToTheMax <gabrielsawicki32@gmail.com>
2026-02-24 18:02:03 +0000

    Updated unity and remaking camera

---
+++ Assets/GabStuff/Scripts/FPSCamera.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace GabStuff.Scripts
+{
+    public class FPSCamera : MonoBehaviour
+    {
+        public float playerDirection;
+    
+        public void OnCameraMove()
+        {
+        
+        }
+    
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
