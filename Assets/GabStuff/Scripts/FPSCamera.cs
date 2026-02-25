08b4f103ec7d2bf1ef3df76b700196adbfb7c17d
GabToTheMax <gabrielsawicki32@gmail.com>
2026-02-25 21:16:02 +0000

    Added first person camera

--- Assets/GabStuff/Scripts/FPSCamera.cs
+++ Assets/GabStuff/Scripts/FPSCamera.cs
@@ -1,24 +1,72 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace GabStuff.Scripts
 {
     public class FPSCamera : MonoBehaviour
     {
-        public float playerDirection;
-    
-        public void OnCameraMove()
-        {
+        [SerializeField] private GameObject cameraGameObject;
+        [SerializeField] private GameObject cameraVertical;
+        [SerializeField] private float cameraSensitivityX;
+        [SerializeField] private float cameraSensitivityY;
+        [SerializeField] private float fieldOfView = 90;
+        [SerializeField] private float smoothing;
+        [HideInInspector] public float playerDirection;
+        private Vector2 _mouseInput;
+        private Vector2 _smoothMouseInput;
+        private Camera _camera;
+        private float _yRotation;
         
+
+        private void Start()
+        {
+            _camera = cameraGameObject.GetComponent<Camera>();
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
         }
-    
-        void Start()
-        {
         
+        public void OnCameraMove(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                 _mouseInput = context.ReadValue<Vector2>();
+            }
         }
 
-        void Update()
+        private void Update()
         {
+            SmoothCamera();
+            RotateX();
+            RotateY();
+            UpdateFov();
+        }
+
+        private void SmoothCamera()
+        {
+            _smoothMouseInput = Vector2.Lerp(_smoothMouseInput, _mouseInput, smoothing);
+        }
+
+        private void UpdateFov()
+        {
+            if (!Mathf.Approximately(_camera.fieldOfView, fieldOfView))
+            {
+                _camera.fieldOfView = fieldOfView;
+            }
+        }
         
+        private void RotateX()
+        {
+            transform.Rotate(0, _smoothMouseInput.x * cameraSensitivityX, 0);
+        }
+
+        private void RotateY()
+        {
+            //cameraGameObject.transform.Rotate(-_smoothMouseInput.y * cameraSensitivityY, 0,0);
+            _yRotation += -_smoothMouseInput.y * cameraSensitivityY;
+            _yRotation = Mathf.Clamp(_yRotation, -90f, 90f);
+            cameraGameObject.transform.rotation = Quaternion.Euler(_yRotation, cameraGameObject.transform.rotation.eulerAngles.y, cameraGameObject.transform.rotation.eulerAngles.z);
+            print(_yRotation);
         }
     }
 }
