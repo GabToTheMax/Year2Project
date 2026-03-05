6051cfb2f115d24a2c31f80a6d1e46e878455be8
gabtothemax <gabrielsawicki32@gmail.com>
2026-03-05 10:18:28 +0000

    Portal rotation working great now

--- Assets/GabStuff/Scripts/PortalScript.cs
+++ Assets/GabStuff/Scripts/PortalScript.cs
@@ -9,16 +9,18 @@ namespace GabStuff.Scripts
         #region Variables
         
         [SerializeField] private GameObject player;
+        [SerializeField] private int debugRotationValue;
         public int index;
         public Vector3 vectorToPlayerCamera;
         private FPSCamera _fpsCam;
         private Portal _thisPortal;
         private Portal _otherPortal;
         private Camera _playerCamera;
+        private Quaternion _portalRotationDifference;
         
         #endregion
     
-        void Awake()
+        private void Awake()
         {
             _thisPortal = new Portal(gameObject);
             PortalManager.Instance.SetPortal(_thisPortal);
@@ -32,7 +34,7 @@ namespace GabStuff.Scripts
             _otherPortal = PortalManager.Instance.Portals[1 - index];
         }
 
-        void Update()
+        private void Update()
         {
             MoveCamera();
             RotateCamera();
@@ -40,37 +42,31 @@ namespace GabStuff.Scripts
         
         private void MoveCamera()
         {
+            _portalRotationDifference = _otherPortal.Object.transform.rotation * Quaternion.Inverse(gameObject.transform.rotation);
+            
             vectorToPlayerCamera = _playerCamera.transform.position - transform.position;
-            vectorToPlayerCamera.y *= -1;
-            // Quaternion black magic to account for rotated portals
-            vectorToPlayerCamera = _otherPortal.Object.transform.rotation * Quaternion.Inverse(transform.rotation) * vectorToPlayerCamera;
-            
-            
-            Vector3 otherPortalPos = _otherPortal.Object.transform.position;
-            _thisPortal.Camera.transform.position = otherPortalPos - vectorToPlayerCamera;
-            
             Debug.DrawLine(transform.position, transform.position + vectorToPlayerCamera, Color.green);
-            //Debug.DrawLine(transform.position, transform.position + transform.rotation * Quaternion.Inverse(_otherPortal.Object.transform.rotation) * vectorToPlayerCamera, Color.orange);
-            Debug.DrawLine(otherPortalPos, otherPortalPos + -vectorToPlayerCamera, Color.orange);
+            
+            vectorToPlayerCamera = Quaternion.AngleAxis(180, _thisPortal.Object.transform.forward) * vectorToPlayerCamera;
+            Vector3 otherPortalPos = _otherPortal.Object.transform.position;
+            
+            Debug.DrawLine(otherPortalPos, otherPortalPos + _portalRotationDifference*vectorToPlayerCamera, Color.orange);
+            
+            // Quaternion black magic to account for rotated portals
+            vectorToPlayerCamera = Quaternion.Inverse(gameObject.transform.rotation) * vectorToPlayerCamera;
+            
+            _thisPortal.Camera.transform.position = otherPortalPos + _otherPortal.Object.transform.rotation * vectorToPlayerCamera;
         }
         
         private void RotateCamera()
         {
             Vector3 vectorToOtherPortalFromCamera = _thisPortal.Camera.transform.position - _otherPortal.Object.transform.position;
-            _thisPortal.Camera.transform.rotation = Quaternion.LookRotation(-vectorToOtherPortalFromCamera, Vector3.up);
-
+            _thisPortal.Camera.transform.rotation = Quaternion.LookRotation(-vectorToOtherPortalFromCamera, _portalRotationDifference*Vector3.up);
         }
 
-
-        /*
-         *  To make the camera work
-         *
-         *  vectorToPlayer = current portal to the player
-         *  camera position = other portal position - vectorToPlayer
-         *
-         *  Make camera look at the other portal.
-         *
-         *
-         */
+        private void ZoomInCamera()
+        {
+            _thisPortal.Camera.WorldToScreenPoint(_otherPortal.Object.transform.position);
+        }
     }
 }
