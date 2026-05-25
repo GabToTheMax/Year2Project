fc2241ebdef7bf40bd022a193ed3ae56faeae249
GabToTheMax <gabrielsawicki32@gmail.com>
2026-05-25 23:38:10 +0100

    Made Player grabber

--- Assets/GabStuff/Scripts/PlayerGrabbable.cs
+++ Assets/GabStuff/Scripts/PlayerGrabbable.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-public class PlayerGrabbable : MonoBehaviour
+namespace GabStuff.Scripts
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PlayerGrabbable : MonoBehaviour
     {
+    
+    
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        void Update()
+        {
         
+        }
     }
 }
